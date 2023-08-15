68fe4927246df24adb9b668182ddb50f08481790
Adeoye Thomas <adeoye.thomas@hotmail.com>
2023-08-15 22:52:17 +0100

    Team ID Grab working storing correct structure

--- team_ppg_scrape.py
+++ team_ppg_scrape.py
@@ -19,38 +19,39 @@ headers = {
     'x-rapidapi-host': 'v1.basketball.api-sports.io'
 }
 
-team_info = team_info()
-team_ids = [k  for  k in  team_info.keys()]
+t_ids_dict = team_info()
+team_ids = [k for k in  t_ids_dict.keys()]
 
+print(team_ids)
 
 
 split = team_ids[:3]
 print(split)
 
-response = requests.request("GET", url, headers=headers, data=payload)
+# response = requests.request("GET", url, headers=headers, data=payload)
 
-# Parses the JSON
-team_statistics = json.loads(response.text)
-# print(json.dumps(team_statistics, indent=4))
+# # Parses the JSON
+# team_statistics = json.loads(response.text)
+# # print(json.dumps(team_statistics, indent=4))
 
-ppg_for = team_statistics["response"]["points"]["for"]["average"]["all"]
-ppg_against = team_statistics["response"]["points"]["against"]["average"]["all"]
-team_name = team_statistics["response"]["team"]["name"]
+# ppg_for = team_statistics["response"]["points"]["for"]["average"]["all"]
+# ppg_against = team_statistics["response"]["points"]["against"]["average"]["all"]
+# team_name = team_statistics["response"]["team"]["name"]
 
-print('=====================================================================================')
+# print('=====================================================================================')
 
-print("Team Name is: " + team_name)
-print(team_name +" " + "scored"+ " " + str(ppg_for) + " " + "ppg")
-print(team_name + " " + "conceded" + " " + str(ppg_against) +" " + "ppg")
+# print("Team Name is: " + team_name)
+# print(team_name +" " + "scored"+ " " + str(ppg_for) + " " + "ppg")
+# print(team_name + " " + "conceded" + " " + str(ppg_against) +" " + "ppg")
 
-print('=====================================================================================')
+# print('=====================================================================================')
 
-# print(json.dumps(response.headers))
+# # print(json.dumps(response.headers))
 
-daily_calls_remaining = response.headers['x-ratelimit-requests-remaining']
-perMinute_calls_remaining = response.headers['X-RateLimit-Remaining']
+# daily_calls_remaining = response.headers['x-ratelimit-requests-remaining']
+# perMinute_calls_remaining = response.headers['X-RateLimit-Remaining']
 
-print('Daily calls remaining = ' + daily_calls_remaining)
-print('Calls left per minute = ' + perMinute_calls_remaining)
+# print('Daily calls remaining = ' + daily_calls_remaining)
+# print('Calls left per minute = ' + perMinute_calls_remaining)
 
-    # time.sleep(7)
+#     # time.sleep(7)
