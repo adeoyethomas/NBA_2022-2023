49a97b9c68802c7b793d620d3c1f52eb3d092c08
Adeoye Thomas <adeoye.thomas@hotmail.com>
2024-01-07 00:44:27 +0000

    Created function that creates df for ppg scored for and against a team

--- (test)team_identifiers_grab.py
+++ (test)team_identifiers_grab.py
@@ -2621,5 +2621,10 @@ def team_id_df_creation(team_lvl):
 team_ids_df = team_id_df_creation(team_lvl)
 print(team_ids_df)
 
+team_ids = []
 
+for index, row in team_ids_df.iterrows():
+    team_ids.append(row['Team ID'])
+
+print(team_ids)
 
